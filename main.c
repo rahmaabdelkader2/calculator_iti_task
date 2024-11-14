3be83962fdaaa43f1b52fe57a5085ca9e581aaba
rahmaabdelkader2 <abdelkaderrahma7@gmail.com>
2024-11-14 12:35:29 +0200

    adding subtraction

--- main.c
+++ main.c
@@ -1,6 +1,20 @@
 #include <stdio.h>
+#include <stdint.h>
+
+int32_t subtraction(int32_t x,int32_t y);
 
 int main(void)
 {
+ int32_t x=0;int32_t y=0;
+ int32_t result;
+ scanf("%d %d",&x,&y);
+ result =subtraction(x,y);
+ printf("%d",result);
+
     return 0;
+}
+
+int32_t subtraction(int32_t x,int32_t y){
+    int32_t result=x-y;
+    return result;
 }
