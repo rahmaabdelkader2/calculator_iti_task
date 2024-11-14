300a54521e84dd9b661bf385701f9c2f7bbb28ea
Abdallah <abdallahsalah74.as@gmail.com>
2024-11-14 12:26:43 +0200

    Adding Sum Function

--- main.c
+++ main.c
@@ -1,6 +1,25 @@
 #include <stdio.h>
 
+int sum(int num1,int num2);
+
 int main(void)
 {
+    int num1,num2;
+
+    printf("Enter the 1st Number: ");
+    scanf("%d",&num1);
+    printf("\n");
+
+    printf("Enter the 2nd Number: ");
+    scanf("%d",&num2);
+    printf("\n");
+
+    printf("SUM = %d\n", sum(num1,num2) );
+
     return 0;
+}
+
+int sum(int num1,int num2)
+{
+    return num1 + num2;
 }
