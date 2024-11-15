c23f6f9c329be033b01c79eb638a35cafb4d5ac9
Patrick Atef <patrickatefffa22@gmail.com>
2024-11-15 19:28:07 +0200

    ModulusTwoNumbers

--- main.c
+++ main.c
@@ -1,45 +1,37 @@
 #include <stdio.h>
-#include <stdint.h>
+#include<stdint.h>
 
-int32_t subtraction(int32_t x, int32_t y);
-int32_t Divide(int32_t Dividend, int32_t Divisor);
-int32_t sum(int32_t num1,int32_t num2);
 
-int main(void)
+void Modulus(int32_t x, int32_t y);
+
+int main()
+{ 
+    int32_t x,y,result=0;
+
+    printf("Enter Two Numbers: ");
+    scanf("%d %d",&x,&y);
+    Modulus(x,y);
+
+
+
+
+}
+
+void Modulus(int32_t x, int32_t y)
 {
-    int32_t x = 0, y = 0;
-    int32_t Res = 0;
+    int Result;
 
+    if (y==0)
+{
+    printf("division by zero is not allowed -__-");
+}
 
-    printf("Enter two numbers: ");
-    scanf("%d %d", &x, &y);
-    Res = subtraction(x, y); 
-    printf("Subtraction Result: %d\n", Res);
-
-  
-    Res = Divide(x, y);
-    printf("%d / %d = %d\n", x, y, Res);
-    printf("Division Result: %d\n", Res);
-	
-	Res = sum(x,y);
-    printf("SUM = %d\n",Res);
+else 
+{
+    Result = x%y;
     
-
-    return 0;
+    printf("%d",Result);
+    
 }
-
-int32_t sum(int32_t num1,int32_t num2)
-{
-    return num1 + num2;
-}
-
-int32_t subtraction(int32_t x, int32_t y) {
-    return x - y;
-}
-
-int32_t Divide(int32_t Dividend, int32_t Divisor) {
-    if (Divisor == 0) {
-        return INT32_MIN; 
-    }
-    return (Dividend / Divisor);
+   
 }
