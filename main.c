6f6756981fc6e079403f113c86f02cce9f6cee24
Patrick Atef <patrickatefffa22@gmail.com>
2024-11-16 02:01:44 +0200

    ModulusFeature

--- main.c
+++ main.c
@@ -1,45 +1,23 @@
 #include <stdio.h>
 #include <stdint.h>
 
-int32_t subtraction(int32_t x, int32_t y);
-int32_t Divide(int32_t Dividend, int32_t Divisor);
-int32_t sum(int32_t num1,int32_t num2);
+void Modulus(int32_t x, int32_t y);
 
-int main(void)
+int main ()
 {
-    int32_t x = 0, y = 0;
-    int32_t Res = 0;
-
-
-    printf("Enter two numbers: ");
-    scanf("%d %d", &x, &y);
-    Res = subtraction(x, y); 
-    printf("Subtraction Result: %d\n", Res);
-
-  
-    Res = Divide(x, y);
-    printf("%d / %d = %d\n", x, y, Res);
-    printf("Division Result: %d\n", Res);
-	
-	Res = sum(x,y);
-    printf("SUM = %d\n",Res);
-    
-
-    return 0;
+ int32_t x,y,result;
+ Modulus(x,y);
 }
 
-int32_t sum(int32_t num1,int32_t num2)
+void Modulus(int32_t x, int32_t y)
 {
-    return num1 + num2;
-}
-
-int32_t subtraction(int32_t x, int32_t y) {
-    return x - y;
-}
-
-int32_t Divide(int32_t Dividend, int32_t Divisor) {
-    if (Divisor == 0) {
-        return INT32_MIN; 
+ if (y==0)
+    {
+        printf("dividing by zero is not allowed -__-");
     }
-    return (Dividend / Divisor);
+    else
+    {
+        int32_t result = x%y;
+         printf("the remainder of %d by %d is %d",x,y,result);
+    }
 }
